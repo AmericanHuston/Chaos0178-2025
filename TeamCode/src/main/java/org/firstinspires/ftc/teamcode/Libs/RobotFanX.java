b0921296a9bb00c0f2b5b95d0b16080d2cd4673a
AmericanHuston <AmericanHuston@gmail.com>
2025-09-26 12:50:18 -0600

    Updated FanXRobot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Libs/RobotFanX.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Libs/RobotFanX.java
@@ -4,15 +4,29 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class RobotFanX {
-    DcMotor frontLeftMotor;
+    double desiredLeftPower = 0.0;
+    double desiredRightPower = 0.0;
     DcMotor backLeftMotor;
-    DcMotor frontRightMotor;
     DcMotor backRightMotor;
 
     public void init(HardwareMap hardwareMap) {
-        frontLeftMotor = hardwareMap.dcMotor.get("frontLeft");
         backLeftMotor = hardwareMap.dcMotor.get("backLeft");
-        frontRightMotor = hardwareMap.dcMotor.get("frontRight");
         backRightMotor = hardwareMap.dcMotor.get("backRight");
+    }
+    public int setDesiredPower(DcMotor motor, double power){
+        if (motor.equals(backLeftMotor)) {
+            desiredLeftPower = power;
+            return 0;
+        } else if (motor.equals(backRightMotor)) {
+            desiredRightPower = power;
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    public void actDrive(){
+        backRightMotor.setPower(desiredRightPower);
+        backLeftMotor.setPower(desiredLeftPower);
     }
 }
