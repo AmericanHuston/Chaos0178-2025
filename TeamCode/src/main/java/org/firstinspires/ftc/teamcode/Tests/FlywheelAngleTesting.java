22e6a287145825490315d66dd86e006d66bf2468
AmericanHuston <americanhuston@gmail.com>
2025-10-09 17:04:02 -0600

    Adding Functionality to FlywheelTesting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/FlywheelAngleTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/FlywheelAngleTesting.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.Tests;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.Libs.NoahHubRobot;
 import org.firstinspires.ftc.teamcode.Libs.Robot3;
 
 @TeleOp(name = "FlywheelAngleTesting", group = "tests")
@@ -12,15 +13,15 @@ public class FlywheelAngleTesting extends LinearOpMode {
         TRIGGER
     }
     flywheelAdjustMode mode = flywheelAdjustMode.DPAD;
-    double flywheelSpeed = 0.00;
-    Robot3 robot = new Robot3();
+    double flywheelPower = 0.00;
+    NoahHubRobot robot = new NoahHubRobot();
 
     @Override
     public void runOpMode() throws InterruptedException {
         robot.init(hardwareMap);
         waitForStart();
         while (opModeIsActive()) {
-            if (gamepad1.a) {
+            if (gamepad1.aWasReleased()) {
                 if (mode == flywheelAdjustMode.TRIGGER) {
                     mode = flywheelAdjustMode.DPAD;
                 } else {
@@ -28,18 +29,27 @@ public class FlywheelAngleTesting extends LinearOpMode {
                 }
             }
             if (gamepad1.right_trigger >= 0.01 && mode == flywheelAdjustMode.TRIGGER) {
-                robot.setDesiredFlywheel(gamepad1.right_trigger);
+                flywheelPower = gamepad1.right_trigger;
             } else if (gamepad1.right_trigger < 0 && mode == flywheelAdjustMode.TRIGGER) {
-                robot.setDesiredFlywheel(0);
+                flywheelPower = 0;
             }
-            if (gamepad1.dpad_down && mode == flywheelAdjustMode.DPAD) {
-                flywheelSpeed -= 0.05;
+            if (gamepad1.dpadDownWasReleased() && mode == flywheelAdjustMode.DPAD) {
+                flywheelPower -= 0.05;
             }
-            if (gamepad1.dpad_up && mode == flywheelAdjustMode.DPAD) {
-                flywheelSpeed += 0.05;
+            if (gamepad1.dpadUpWasReleased() && mode == flywheelAdjustMode.DPAD) {
+                flywheelPower += 0.05;
             }
+            while (gamepad1.b){
+                robot.setFlywheelVelocity(gamepad1.left_trigger * 6000);
+                telemetry.addData("Power", robot.getFlywheelActualPower());
+                telemetry.addData("Speed", (robot.getFlywheelVelocity() * 17.6470588) + " RPM (Approx)"); //Approx RPM
+                telemetry.addData("Mode", "VELOCITY");
+                telemetry.update();
+            }
+            robot.setDesiredFlywheel(flywheelPower);
             robot.actMotors();
-            telemetry.addData("Speed", flywheelSpeed);
+            telemetry.addData("Power", flywheelPower);
+            telemetry.addData("Speed", (robot.getFlywheelVelocity() * 17.6470588) + " RPM (Approx)"); //Approx RPM
             telemetry.addData("Mode", mode.toString());
             telemetry.update();
             if (gamepad1.left_bumper){
