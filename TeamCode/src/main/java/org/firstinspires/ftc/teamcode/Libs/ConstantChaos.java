3ff9ee01a3d14ad78a82a9579266aa6c00f231d9
Lumennnnnnn <serackliffe@gmail.com>
2026-02-19 17:16:37 -0700

    calcpowerflywheel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Libs/ConstantChaos.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Libs/ConstantChaos.java
@@ -15,7 +15,7 @@ public class ConstantChaos {
     public static double maxDistance = 100;
     public static double minDistance = 40;
     public static double maxVelocity = 1650;
-    public static double minVelocity =  1100;
+    public static double minVelocity =  1600;
 
     public static double flyVel = 1575;
 
